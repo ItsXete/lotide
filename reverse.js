96a64d319935b9c288f24a60cc3ed2f437c24596
ItsXete <[dgiabach2000@gmail.com]>
2025-06-23 20:12:15 -0400

    reverse it

---
+++ reverse.js
@@ -0,0 +1,12 @@
+const args = process.argv.slice(2);
+
+for (const word of args) {
+  let reversed = "";
+
+
+for (let i = word.length -1; i >= 0; i--) {
+  reversed += word[i];
+}
+
+console.log(reversed);
+}
