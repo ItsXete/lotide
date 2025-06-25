d6387cd4826ae18e0db63e2fb2bc98bbb6e54183
ItsXete <[dgiabach2000@gmail.com]>
2025-06-25 19:58:00 -0400

    added findKeyByValue

---
+++ findKeyByValue.js
@@ -0,0 +1,29 @@
+// assertEqual
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); 
+
+// findKeyByValue
+const findKeyByValue = function(object, value) {
+  for (const key in object) {
+    if (object[key] === value) {
+      return key;
+    }
+  }
+  return undefined;}
+
+  // TEST
+  const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire",
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
