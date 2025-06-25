0d081eaddb80880b272dd0f0f46d36337dbe9989
ItsXete <[dgiabach2000@gmail.com]>
2025-06-25 19:52:11 -0400

    added letterPosition

---
+++ letterPositions.js
@@ -0,0 +1,39 @@
+// eqArrays
+function eqArrays(arr1, arr2) {
+  if (arr1.length !== arr2.length) return false;
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) return false;
+  }
+
+   return true;
+}
+
+// assertEqual
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); 
+
+// letterPositions
+const letterPositions = function(sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    const char = sentence[i];
+    if (char !== " ") {
+      if (!results[char]) {
+        results[char] = [];
+      }
+      results[char].push(i);
+    }
+  }
+  return results;
+};
+
+// TEST
+assertArraysEqual(letterPositions("hello").e, [1]);
