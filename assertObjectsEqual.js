72c586d7304b45545897e9f9685355fa6c6bf175
ItsXete <[dgiabach2000@gmail.com]>
2025-06-25 20:28:10 -0400

    added assertObjectEqual

---
+++ assertObjectsEqual.js
@@ -0,0 +1,34 @@
+// eqArrays
+function eqArrays(arr1, arr2) {
+  if (arr1.length !== arr2.length) return false;
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) return false;
+  }
+
+   return true;
+}
+
+// assertEqual
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); 
+
+// assertObjectsEqual
+const assertObjectsEqual = function (actual, expected) {
+  const inspect = require('util').inspect;
+
+  if (eqObjects(actual, expected)) {
+    console.log(`✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+  } 
+}
+
+// TEST
