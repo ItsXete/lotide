9af5d03eb422cfba4272eae4c0e58a7cf089d296
ItsXete <[dgiabach2000@gmail.com]>
2025-06-30 16:15:13 -0400

    added test and reff assertEqual n head

--- head.js
+++ head.js
@@ -1,19 +1,9 @@
 // FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require("./assertEqual");
 
-// Head funcion
-
+// Head function
 const head = function(array) {
   return array[0];
 };
 
-// Test
-
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
