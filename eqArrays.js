a657b9d965400bad71b8ec11aca4d50ce01e3b0f
ItsXete <[dgiabach2000@gmail.com]>
2025-06-30 16:21:39 -0400

    added test and reff eqArray

--- eqArrays.js
+++ eqArrays.js
@@ -1,5 +1,5 @@
-// eqArrays
-function eqArrays(arr1, arr2) {
+// eqArrays.js
+const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) return false;
 
   for (let i = 0; i < arr1.length; i++) {
@@ -14,12 +14,6 @@ function eqArrays(arr1, arr2) {
   }
 
   return true;
-}
-
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
 };
+
+module.exports = eqArrays;
